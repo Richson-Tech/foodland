43d16c8af739b9edb8da7ff83e16269ae7ae55eb
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-21 23:55:24 +0100

    cursor pointer added

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Notifications = () => {
   return (
-    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm'>Free delivery for all orders over $150. Order Now!</div>
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Free delivery for all orders over $150. Order Now!</div>
   )
 }
 
