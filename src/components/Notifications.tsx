7e193a559c211af162454fe277de911abd280713
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-21 23:50:13 +0100

    new commit

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Notifications = () => {
   return (
-    <div>Notifications</div>
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm'>Free delivery for all orders over $150. Order Now!</div>
   )
 }
 
-export default Notifications
+export default Notifications  
