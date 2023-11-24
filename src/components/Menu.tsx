801588cf34abc5f07cf1a8c1edc90d3f7f68af1b
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-24 21:33:58 +0100

    new commits

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 const Menu = () => {
   return (
     <div>
+        <Image src="/open.png" alt='' width={20} height={20}/>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Menu from './Menu'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-12 text-red-500 flex items-center justify-between'>
+      {/* LOGO  */}
+      <div>
+        <Link href="/">FOODLAND</Link>
+      </div>
+      {/* mobile menu */}
+
+      <div>
+        <Menu/>
+      </div>
+    </div>
   )
 }
 
