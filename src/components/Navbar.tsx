5fdf3afdacf8440f4a24426346091998385ab1af
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-25 13:53:21 +0100

    close icon added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className='h-12 text-red-500 flex items-center justify-between'>
+    <div className='h-12 text-red-500 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
       {/* LOGO  */}
-      <div>
-        <Link href="/">FOODLAND</Link>
+      <div className='text-xl'>
+        <Link href="/">Foodland</Link>
       </div>
       {/* mobile menu */}
 
