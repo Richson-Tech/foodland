026e765640236e11fdf2d270b0a19bbf2e7bbafe
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-25 21:54:41 +0100

    open icon initiated

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import Link from "next/link";
+import CartIcon from "./CartIcon";
 
 const links = [
   { id: 1, title: "Homepage", url: "/" },
@@ -34,14 +35,37 @@ const Menu = () => {
           onClick={() => setOpen(false)}
         />
       )}
-      <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
-        {links.map((item) => (
-          <Link href={item.url} key={item.id}>
-            {item.title}
+      {open && (
+        <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
+          {links.map((item) => (
+            <Link href={item.url} key={item.id} onClick={() => setOpen(false)}>
+              {item.title}
+            </Link>
+          ))}
+
+          {/* LONG WAY */}
+          {/* {!user ? (
+            <Link href="/login" onClick={() => setOpen(false)}>
+              Login
+            </Link>
+          ) : (
+            <Link href="/orders" onClick={() => setOpen(false)}>
+              Orders
+            </Link>
+          )} */}
+
+          {/* SHORTCUT */}
+          <Link
+            href={user ? "/orders" : "login"}
+            onClick={() => setOpen(false)}
+          >
+            {user ? "Orders" : "Login"}
           </Link>
-        ))}
-        {!user ? <Link href="">Login</Link> : <Link href="">Orders</Link>}
-      </div>
+          <Link href="/cart" onClick={() => setOpen(false)}>
+            <CartIcon />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
