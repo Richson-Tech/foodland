3847827c769f15037d12ab8c8e8740f74013594e
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-25 15:07:54 +0100

    temporary user createdd

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,9 +2,19 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
+import Link from "next/link";
+
+const links = [
+  { id: 1, title: "Homepage", url: "/" },
+  { id: 2, title: "Menu", url: "/menu" },
+  { id: 3, title: "Working Hours", url: "/" },
+  { id: 4, title: "Contact", url: "/" },
+];
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
+  // TEMPORARY USER 
+  const user = false;
   return (
     <div>
       {!open ? (
@@ -24,6 +34,15 @@ const Menu = () => {
           onClick={() => setOpen(false)}
         />
       )}
+      <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
+        {links.map((item) => (
+          <Link href={item.url} key={item.id}>
+            {item.title}
+          </Link>
+        ))}
+        <Link href="">Login</Link>
+        <Link href="">Orders</Link>
+      </div>
     </div>
   );
 };
