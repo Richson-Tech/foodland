a8accd3f03998bb1c485373c5afa7b3d515c9477
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-26 08:55:45 +0100

    done with the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,15 +7,15 @@ import Image from "next/image";
 const Navbar = () => {
   const user = false;
   return (
-    <div className="h-12 text-red-500 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24">
+    <div className="h-12 text-red-500 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
       {/* LEFT LINKS  */}
-      <div className="hidden md:flex gap-4">
+      <div className="hidden md:flex gap-4 flex-1">
         <Link href="/">Homepage</Link>
         <Link href="/menu">Menu</Link>
         <Link href="/">Contact</Link>
       </div>
       {/* LOGO  */}
-      <div className="text-xl">
+      <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">Foodland</Link>
       </div>
       {/* mobile menu */}
@@ -24,8 +24,8 @@ const Navbar = () => {
         <Menu />
       </div>
       {/* RIGHT LINKS  */}
-      <div className="hidden md:flex gap-4 items-center">
-        <div className="flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
           <Image src="/phone.png" alt="phone icon" width={20} height={20}/>
           <span>+234 28469727</span>
         </div>
