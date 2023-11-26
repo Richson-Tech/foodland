968c1885c2578ab6efe94ed736db3693439d6bab
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-26 20:23:08 +0100

    slider imsge added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Slider from '@/components/Slider'
 import Featured from '@/components/Featured'
-import Image from 'next/image'
 import Offer from '@/components/Offer'
 
 export default function Home() {
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Slider = () => {
   return (
-    <div>Slider</div>
-  )
-}
+    <div className="flex flex-col h-screen">
+        {/* TEXT CONTAINER  */}
+      <div className="h-1/2">
+        <h1 className="">
+            Test
+        </h1>
+        <button>Order Now</button>
+      </div>
+        {/* IMAGE CONTAINER  */}
+      <div className="w-full h-1/2 relative">
+        <Image src="/slide1.png" alt="slider image" fill/>
+      </div>
+    </div>
+  );
+};
 
-export default Slider
+export default Slider;
