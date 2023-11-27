c5044fc753c96fea598b41873737b34f69b41996
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-27 22:27:50 +0100

    added slider interval functions

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,38 +1,51 @@
+"use client";
 import Image from "next/image";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 
 const data = [
-    {
-      id: 1,
-      title: "always fresh & always crispy & always hot",
-      image: "/slide1.png",
-    },
-    {
-      id: 2,
-      title: "we deliver your order wherever you are in NY",
-      image: "/slide2.png",
-    },
-    {
-      id: 3,
-      title: "the best pizza to share with your family",
-      image: "/slide3.jpg",
-    },
-  ];
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    image: "/slide1.png",
+  },
+  {
+    id: 2,
+    title: "we deliver your order wherever you are in NY",
+    image: "/slide2.png",
+  },
+  {
+    id: 3,
+    title: "the best pizza to share with your family",
+    image: "/slide3.jpg",
+  },
+];
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      () => setCurrentSlide((prev) => prev + 1);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
-        {/* TEXT CONTAINER  */}
+      {/* TEXT CONTAINER  */}
       <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
         <h1 className="text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl">
-            Test
+          {data[currentSlide].title}
         </h1>
         <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
       </div>
-        {/* IMAGE CONTAINER  */}
+      {/* IMAGE CONTAINER  */}
       <div className="w-full h-1/2 relative">
-        <Image src="/slide1.png" alt="slider image" fill className="object-cover"/>
+        <Image
+          src={data[currentSlide].image}
+          alt="slider image"
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
   );
