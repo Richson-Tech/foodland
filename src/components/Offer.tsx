f0351ffa1c0a40b57d724c1d4c6245da25956a8a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-28 23:14:22 +0100

    more commits

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -3,12 +3,26 @@ import Image from "next/image";
 
 const Offer = () => {
   return (
-    <div className="">
+    <div className="bg-black h-screen flex flex-col md:flex-row">
       {/* TEXT CONTAINER  */}
-      <div className=""></div>
+      <div className=" flex-1">
+        <h1 className="text-white text-5xl font-bold xl:text-6xl">
+          Delicious Burger & French Fry
+        </h1>
+        <p className="text-white">
+          Progressively simplify effective e-toilers and process-centric methods
+          of empowermwnt. Quickly potificate parallel.
+        </p>
+        <button className="">Order Now</button>
+      </div>
       {/* TEXT CONTAINER  */}
-      <div className="relative">
-        <Image src="/offerProduct.png" alt="offer pic" fill/>
+      <div className="flex-1 w-full relative">
+        <Image
+          src="/offerProduct.png"
+          alt="offer pic"
+          fill
+          className="object-contain"
+        />
       </div>
     </div>
   );
