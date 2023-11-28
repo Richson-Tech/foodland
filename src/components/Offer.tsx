bd1d0a3e60f06d9da3765105b28645b5712e90f0
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-28 23:32:33 +0100

    all styles added to the offer file

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -5,15 +5,15 @@ const Offer = () => {
   return (
     <div className="bg-black h-screen flex flex-col md:flex-row">
       {/* TEXT CONTAINER  */}
-      <div className=" flex-1">
+      <div className=" flex-1 flex-col justify-center items-center text-center gap-8 p-6">
         <h1 className="text-white text-5xl font-bold xl:text-6xl">
           Delicious Burger & French Fry
         </h1>
-        <p className="text-white">
+        <p className="text-white xl:text-xl">
           Progressively simplify effective e-toilers and process-centric methods
           of empowermwnt. Quickly potificate parallel.
         </p>
-        <button className="">Order Now</button>
+        <button className="bg-red-500 text-white rounded-md py-3 px-6">Order Now</button>
       </div>
       {/* TEXT CONTAINER  */}
       <div className="flex-1 w-full relative">
