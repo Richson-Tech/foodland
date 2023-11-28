2968224c5c28a332ed898fcaf4cddc4fd70a48f1
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-28 01:24:15 +0100

    condition statement added to the types on the image

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,3 +1,4 @@
+import { featuredProducts } from "@/data";
 import Image from "next/image";
 import React from "react";
 
@@ -7,19 +8,30 @@ const Featured = () => {
       {/* WRAPPER  */}
       <div className="w-max flex">
         {/* SINGLE ITEM  */}
-        <div className="w-screen h-[60vh] flex flex-col items-center justify-around">
-          {/* IMAGE CONTAINER  */}
-          <div className="relative flex-1 w-full">
-            <Image src="/temporary/p1.png" alt="temporary img" fill className="object-contain p-4"/>
+        {featuredProducts.map((items) => (
+          <div key={items.id} className="w-screen h-[60vh] flex flex-col items-center justify-around">
+            {/* IMAGE CONTAINER  */}
+            {items.img && (
+                <div className="relative flex-1 w-full">
+              <Image
+                src={items.img}
+                alt="temporary img"
+                fill
+                className="object-contain p-4"
+              />
+            </div>
+            )}
+            {/* TEXT CONTAINER  */}
+            <div className="flex-1 flex flex-col gap-4">
+              <h1 className="text-xl font-bold uppercase">{items.title}</h1>
+              <p>{items.desc}</p>
+              <span className="text-xl font-bold">{items.price}</span>
+              <button className="bg-red-500 text-white p-2 rounded-md">
+                Add to Cart
+              </button>
+            </div>
           </div>
-          {/* TEXT CONTAINER  */}
-          <div className="flex-1 flex flex-col gap-4">
-            <h1 className="text-xl font-bold uppercase">Title</h1>
-            <p>Desc</p>
-            <span className="text-xl font-bold">$123</span>
-            <button className="bg-red-500 text-white p-2 rounded-md">Add to Cart</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
