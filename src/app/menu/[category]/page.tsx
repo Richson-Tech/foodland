49566c9f0f87a43b4015b860837b55b0795b53c9
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-12-03 22:30:42 +0100

    working on category change

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -7,14 +7,25 @@ const CategoryPage = () => {
   return (
     <div className="flex flex-wrap text-red-500">
       {pizzas.map((item) => (
-        <Link className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3" href={`product/${item.id}`} key={item.id}>
+        <Link
+          className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 flex flex-col justify-between group even:bg-fuchsia-50"
+          href={`product/${item.id}`}
+          key={item.id}
+        >
           {/* IMAGE CONTAINER  */}
           {item.img && (
             <div className="relative h-[80%]">
-              <Image src={item.img} alt="img" fill />
+              <Image src={item.img} alt="img of products" fill />
             </div>
           )}
           {/* Text CONTAINER  */}
+          <div className="flex items-start justify-between font-bold">
+            <h1 className="2xl uppercase p-2">{item.title}</h1>
+            <h1 className="group-hover:hidden text-xl">${item.price}</h1>
+            <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">
+              Add to Cart
+            </button>
+          </div>
         </Link>
       ))}
     </div>
