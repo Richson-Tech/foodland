0cfdb01bf5a4a1e78de3b310ea84558c784d4f34
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-12-04 22:50:46 +0100

    started working on cart

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
-  )
-}
+    <div className="">
+      {/* PRODUCT CONTAINER */}
+      <div className="">
+        {/* SINGLE ITEM  */}
+        <Image src="/temporary/p1.png" alt="item image" fill/>
+        
+      </div>
+      <div className="">
+        {/* PAYMENT CONTAINER */}
+      </div>
+    </div>
+  );
+};
 
-export default CartPage
+export default CartPage;
