01dc92a43a2c664288e6930be23b20c28283cabe
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-12-04 23:29:06 +0100

    tried adding a favicon but failled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "FOODLAND",
   description: "A food app",
+  
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import Slider from "@/components/Slider";
 import Featured from "@/components/Featured";
 import Offer from "@/components/Offer";
 
+
+
 export default function Home() {
+  
   return (
     <main>
       <Slider />
