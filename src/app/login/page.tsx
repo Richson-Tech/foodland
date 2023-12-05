9889ef77365b55a364910dff7e45de21eae47561
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-12-05 22:43:38 +0100

    facebook button added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,12 +8,32 @@ const LoginPage = () => {
       <div className="">
         {/* IMAGE CONTAINER  */}
         <div className="relative">
-       <Image src="/loginBg.png" alt="login image" fill/>
+          <Image src="/loginBg.png" alt="login image" fill />
         </div>
         {/* FROM CONTAINER  */}
         <div className="">
-        <h1>Welcome</h1>
-        <p>Log into your account or create a new one using social buttons</p>
+          <h1>Welcome</h1>
+          <p>Log into your account or create a new one using social buttons</p>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+            <Image
+              src="/google.png"
+              alt="google image"
+              height={20}
+              width={20}
+              className="object-contain"
+            />
+            <span>Sign in with Google</span>
+          </button>
+          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+            <Image
+              src="/facebook.png"
+              alt="facebook image"
+              height={20}
+              width={20}
+              className="object-contain"
+            />
+            <span>Sign in with Facebook</span>
+          </button>
         </div>
       </div>
     </div>
