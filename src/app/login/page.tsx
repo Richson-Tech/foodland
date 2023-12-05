dfbcc68c4837d52bdefb4fefe151b9b0c5148196
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-12-05 22:30:09 +0100

    started working on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="">
+      {/* BOX  */}
+      <div className="">
+        {/* IMAGE CONTAINER  */}
+        <div className="relative">
+       <Image src="/loginBg.png" alt="login image" fill/>
+        </div>
+        {/* FROM CONTAINER  */}
+        <div className="">
+        <h1>Welcome</h1>
+        <p>Log into your account or create a new one using social buttons</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
