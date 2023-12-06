c0f9b61f02222f0b819840e0a5bcfbd8fe85fb7a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-12-06 15:22:03 +0100

    amended footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,15 +10,20 @@ const Footer = () => {
   }, []);
 
   return (
-    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-[#00A082] flex items-center justify-between">
-      <Link href="/" className="text-xl font-bold">
+    <div className="flex items-center justify-between h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-[#00A082] ">
+      <Link
+        href="/"
+        className="text-xl font-bold md:text-lg lg:text-xl xl:text-2xl"
+      >
         FOODLAND
       </Link>
-      <p className="flex flex-row items-center justify-center text-base mt-20 font-['Work_Sans'] ">
-        Copyright<sup className="text-base font-bold">&copy;</sup> All Rights
-        Reserved{" "}
-        <span className="font-semibold pl-2">
-          {" "}
+      <p className=" flex flex-col items-center justify-center text-base mt-20 font-['Work_Sans'] md:text-lg lg:text-xl xl:text-2xl ">
+        <div className="flex flex-row">
+
+        All Rights Reserved
+        <sup className="text-base font-bold">&copy;</sup>
+        </div>
+        <span className="flex flex-row text-sm font-semibold pl-2">
           RICHSON TECH. <span className="pl-2">{Year}</span>{" "}
         </span>
       </p>
