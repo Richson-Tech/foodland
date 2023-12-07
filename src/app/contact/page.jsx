98aab3d6bc98789b50b09e645df8a1d1188afa1f
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-12-07 15:36:01 +0100

    contact page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div className="flex items-center justify-center mt-10">
+      <h1>COMING SOON</h1>
+    </div>
+  );
+};
+
+export default ContactPage;
