6b1b854fd917142ab4f5bf8e69819bdba6027b41
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-01-19 22:03:18 +0100

    auth utils created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "FOODLAND",
   description: "The Paradise of Burgers and Pizzas",
-  
 };
 
 export default function RootLayout({
@@ -21,10 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AuthProvider>
+
         <Notifications />
         <Navbar />
         {children}
         <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
