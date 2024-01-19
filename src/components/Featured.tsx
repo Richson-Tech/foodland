42d22a76397de25c1c4a79ec945b68ac08246a36
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-01-19 16:19:58 +0100

    types added for products

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,8 +1,19 @@
-import { featuredProducts } from "@/data";
 import Image from "next/image";
 import React from "react";
 
-const Featured = () => {
+const getData = async ()=> {
+  const res = await fetch("http://localhost:3001/api/categories", {
+    cache:"no-store"
+  })
+  if(!res.ok) {
+    throw new Error("Failled !");
+  }
+  return res.json();
+}
+
+
+const Featured = async () => {
+  const featuredProducts = await getData();
   return (
     <div className="w-screen overflow-x-scroll text-black">
       {/* WRAPPER  */}
