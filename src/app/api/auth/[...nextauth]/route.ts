ee446c1b4f8d2a9fd9cef50de13ef0a8ffc2be29
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-01-24 11:25:47 +0100

    new commkits

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,4 +2,4 @@ import { authOptions } from "@/utils/auth";
 import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions)
-export {handler as GET, handler as PORT}
+export {handler as GET, handler as POST}
